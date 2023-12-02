9568df34bc5f6187ae45e70e1911a685fc8baf71
SiowYenC <Clairechong998@gmail.com>
2023-12-02 16:36:26 +0800

    lab 8-speech translation update

--- 08-speech-translation/C-Sharp/translator/Program.cs
+++ 08-speech-translation/C-Sharp/translator/Program.cs
@@ -5,7 +5,10 @@ using System.Collections.Generic;
 using System.Text;
 
 // Import namespaces
-
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+using Microsoft.CognitiveServices.Speech.Translation;
+using System.Media;
 
 namespace speech_translation
 {
@@ -18,6 +21,7 @@ namespace speech_translation
         {
             try
             {
+                //Prepare to use the Azure AI Speech Translation service
                 // Get config settings from AppSettings
                 IConfigurationBuilder builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
                 IConfigurationRoot configuration = builder.Build();
@@ -26,10 +30,15 @@ namespace speech_translation
 
 
                 // Configure translation
-
+                translationConfig = SpeechTranslationConfig.FromSubscription(cogSvcKey, cogSvcRegion);
+                translationConfig.SpeechRecognitionLanguage = "en-US";
+                translationConfig.AddTargetLanguage("fr");
+                translationConfig.AddTargetLanguage("es");
+                translationConfig.AddTargetLanguage("hi");
+                Console.WriteLine("Ready to translate from " + translationConfig.SpeechRecognitionLanguage);
 
                 // Configure speech
-                
+                speechConfig = SpeechConfig.FromSubscription(cogSvcKey, cogSvcRegion);
 
                 string targetLanguage = "";
                 while (targetLanguage != "quit")
@@ -57,10 +66,29 @@ namespace speech_translation
             string translation = "";
 
             // Translate speech
-
+            using AudioConfig audioConfig = AudioConfig.FromDefaultMicrophoneInput();
+            using TranslationRecognizer translator = new TranslationRecognizer(translationConfig, audioConfig);
+            Console.WriteLine("Speak now...");
+            TranslationRecognitionResult result = await translator.RecognizeOnceAsync();
+            Console.WriteLine($"Translating '{result.Text}'");
+            translation = result.Translations[targetLanguage];
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine(translation);
 
             // Synthesize translation
-
+            var voices = new Dictionary<string, string>
+                            {
+                                ["fr"] = "fr-FR-HenriNeural",
+                                ["es"] = "es-ES-ElviraNeural",
+                                ["hi"] = "hi-IN-MadhurNeural"
+                            };
+            speechConfig.SpeechSynthesisVoiceName = voices[targetLanguage];
+            using SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer(speechConfig);
+            SpeechSynthesisResult speak = await speechSynthesizer.SpeakTextAsync(translation);
+            if (speak.Reason != ResultReason.SynthesizingAudioCompleted)
+            {
+                Console.WriteLine(speak.Reason);
+            }
 
         }
 
